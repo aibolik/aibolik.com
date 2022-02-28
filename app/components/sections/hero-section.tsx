a622cd6545d39665d9e1cfdb7a3ae496c348164f
Aibol Kussain <aibolikdev@gmail.com>
2022-02-28 22:12:46 +0000

    feat(homepage): implement basic v1 hero section

--- app/components/sections/hero-section.tsx
+++ app/components/sections/hero-section.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+import { themeGet } from '~/utils/theme-get';
 import { MaxWidthWrapper } from '../max-width-wrapper';
 
 const HeaderWrapper = styled.header`
@@ -8,15 +9,88 @@ const HeaderWrapper = styled.header`
 
 const Wrapper = styled.div`
   display: flex;
-  flex-direction: columns;
-  align-items: center;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
   min-height: 390px;
+
+  @media ${themeGet('breakpoints.mobile')} {
+    margin-bottom: 48px;
+  }
 `;
 
 const Heading = styled.h1`
   color: var(--color-heading-text);
   font-weight: var(--font-weight-regular);
   font-size: 4rem;
+  margin-bottom: 32px;
+`;
+
+const IntroText = styled.div`
+  font-size: 2rem;
+  line-height: 1.6;
+  max-width: 690px;
+`;
+
+const waving = keyframes`
+
+  12% {
+    transform: rotate(-10deg);
+  }
+
+  24% {
+    transform: rotate(10deg);
+  }
+
+  36% {
+    transform: rotate(-10deg);
+  }
+
+  48% {
+    transform: rotate(0deg);
+  }
+
+  100% {
+    transform: rotate(0deg);
+  }
+`;
+
+const Wave = styled.span`
+  display: inline-block;
+  transform-origin: bottom right;
+  animation: ${waving} 3s ease-in-out 2s infinite;
+`;
+
+const Creative = styled.span`
+  border-radius: 6px;
+  background: var(--red9);
+  padding: 0 6px;
+`;
+
+const Experiment = styled.span`
+  font-style: italic;
+  font-weight: var(--font-weight-semibold);
+  padding: 0 6px;
+  border-radius: 6px;
+  background: var(--purple9);
+`;
+
+const Learn = styled.span`
+  border-radius: 6px;
+  padding: 0 6px;
+  background: var(--grass9);
+  font-style: italic;
+  font-weight: var(--font-weight-semibold);
+`;
+
+
+const Share = styled.span`
+  display: inline-block;
+  padding: 0 6px;
+  border-radius: 6px;
+  background: var(--blue9);
+  font-style: italic;
+  font-weight: var(--font-weight-semibold);
 `;
 
 interface HeroSectionProps {
@@ -33,8 +107,11 @@ const HeroSection: React.FC<HeroSectionProps> = ({
       <MaxWidthWrapper>
         <Wrapper>
           <Heading>
-            Some hero content
+            Hello, <Wave>👋</Wave>!
           </Heading>
+          <IntroText>
+            Welcome to my <Creative>creative</Creative> space where I <Experiment>experiment</Experiment>, <Learn>learn</Learn> and <Share>share</Share>!
+          </IntroText>
         </Wrapper>
       </MaxWidthWrapper>      
     </HeaderWrapper>
